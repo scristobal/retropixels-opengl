98586f434f3379987996a0be5158d099e134358d
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-12-26 09:37:14 +0100

    Retab

--- src/main.cpp
+++ src/main.cpp
@@ -9,65 +9,65 @@
 
 static void error_callback(int error, const char* description)
 {
-  fprintf(stderr, "Error: %s\n", description);
+    fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-  {
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
-  }
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
 }
 
 int main() {
 
-  glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(error_callback);
 
-  if (!glfwInit())
-  {
-    exit(EXIT_FAILURE);
-  }
+    if (!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(640, 480, "RetroPixel", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "RetroPixel", NULL, NULL);
 
-  if (!window) {
-    goto cleanup; 
-  }
+    if (!window) {
+        goto cleanup; 
+    }
 
-  glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, key_callback);
 
-  glfwMakeContextCurrent(window);
-  if (!gladLoadGL(glfwGetProcAddress)){
-    goto cleanup;
-  }
-  glfwSwapInterval(1);
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)){
+        goto cleanup;
+    }
+    glfwSwapInterval(1);
 
 
-  while (!glfwWindowShouldClose(window))
-  {
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
 
-    glViewport(0,0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT);
+        glViewport(0,0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
 cleanup:
-  if (window) {
-    glfwDestroyWindow(window);
-  }
+    if (window) {
+        glfwDestroyWindow(window);
+    }
 
-  glfwTerminate();
+    glfwTerminate();
 
-  exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
 
 
