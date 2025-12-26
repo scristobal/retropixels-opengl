183257c562f150e196c54db2c96c487ad741b6aa
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-12-26 09:28:46 +0100

    Blank window

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,17 @@
 #include <cstdlib>
 
 
-void error_callback(int error, const char* description)
+static void error_callback(int error, const char* description)
 {
   fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
 }
 
 int main() {
@@ -21,24 +29,41 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   GLFWwindow* window = glfwCreateWindow(640, 480, "RetroPixel", NULL, NULL);
 
   if (!window) {
-    glfwTerminate();
-    exit(EXIT_FAILURE);
+    goto cleanup; 
   }
 
+  glfwSetKeyCallback(window, key_callback);
+
   glfwMakeContextCurrent(window);
-  gladLoadGL(glfwGetProcAddress);
+  if (!gladLoadGL(glfwGetProcAddress)){
+    goto cleanup;
+  }
+  glfwSwapInterval(1);
 
 
   while (!glfwWindowShouldClose(window))
   {
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
 
+    glViewport(0,0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
   }
 
-
-  glfwDestroyWindow(window);
+cleanup:
+  if (window) {
+    glfwDestroyWindow(window);
+  }
 
   glfwTerminate();
 
